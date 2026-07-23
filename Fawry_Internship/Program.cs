c6573185708672463919f5c4405e9b8e8ad15e48
MODY <mohmmedelsawy68@gmail.com>
2026-07-24 00:06:49 +0300

    Add project files.

---
+++ Fawry_Internship/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fawry_Internship
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
